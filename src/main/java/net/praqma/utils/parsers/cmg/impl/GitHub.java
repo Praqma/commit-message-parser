f8048bc41d465ff55b3a375b3044153075d86f23
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-07-20 20:22:14 +0100

    redo GitHub regex, add support for revert, extend test suite
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- src/main/java/net/praqma/utils/parsers/cmg/impl/GitHub.java
+++ src/main/java/net/praqma/utils/parsers/cmg/impl/GitHub.java
@@ -16,12 +16,13 @@ import java.util.regex.Pattern;
 // Based on rules described here https://help.github.com/articles/closing-issues-via-commit-messages/
 public class GitHub implements CommitMessageParser {
     private static final Logger log = Logger.getLogger(GitHub.class.getName());
-    private static final Pattern issuePattern = Pattern.compile("([A-Za-z]+)(\\s+)(([A-Za-z0-9\\_\\-]*/?[A-Za-z0-9\\_\\-]*)(#{1}[0-9]+))", Pattern.MULTILINE);
-    private static final List<String> magicWords = Arrays.asList("close", "closes", "closed", "fix", "fixes", "fixed", "resolve", "resolves", "resolved");
+    private static final Pattern issuePattern = Pattern.compile("(?<magicWord>[A-Za-z]+)?(?:[\\s+&&[^\\n]])*?(?<otherProject>[A-Za-z0-9\\_\\-]*/?[A-Za-z0-9\\_\\-]*)?(?<issue>(#{1}[0-9]+))", Pattern.MULTILINE);
+    private static final List<String> closingMagicWords = Arrays.asList("close", "closes", "closed", "fix", "fixes", "fixed", "resolve", "resolves", "resolved");
+    private static final List<String> revertingMagicWords = Arrays.asList("revert", "reopen");
     private URL baseUrl = null;
     private String baseProject = null;
 
-    // Base URL is url to GitHub instance (public source or enterprise)
+    // Base URL is url to GitHub instance (public or enterprise)
     // Base project is a project name user/repo
     public GitHub (final URL baseUrl, final String baseProject) {
         this.baseUrl = baseUrl;
@@ -35,25 +36,28 @@ public class GitHub implements CommitMessageParser {
         while (matcher.find()) {
             // We expect 3 groups to be found
             // #1 Contains magic word
-            // #2 Contains spaces between magic word and issue
-            // #3 Contains full issue number including another repo (ex. user/repo#issue) if present
-            // #4 Contains another repo name if present
+            // #2 Contains full issue number including another repo (ex. user/repo#issue) if present
+            // #3 Contains another repo name if present
             log.fine("Match! Found the following:");
             for (int groupNumber = 0; groupNumber < matcher.groupCount(); groupNumber += 1) {
                 log.fine("group " + groupNumber + ": " + matcher.group(groupNumber));
             }
-            log.fine("Set transition type to " + TransitionType.REFERENCE);
             TransitionType type = TransitionType.REFERENCE;
-            if (magicWords.contains(matcher.group(1).toString().toLowerCase())) {
-                log.fine("Found magic word " + matcher.group(1) + "; Set transition type to " + TransitionType.RESOLVE);
-                type = TransitionType.RESOLVE;
+            if (matcher.group("magicWord") != null && ! matcher.group("magicWord").isEmpty()) {
+                final String magicWord = matcher.group("magicWord");
+                if (closingMagicWords.contains(magicWord.toLowerCase())) {
+                    type = TransitionType.RESOLVE;
+                }
+                if (revertingMagicWords.contains(magicWord.toLowerCase())) {
+                    type = TransitionType.REVERT;
+                }
             }
-            final String issueNumber = matcher.group(3).split("#")[1];
+            final String issueNumber = matcher.group("issue").split("#")[1];
             URL url = null;
-            if (matcher.group(4).toString().isEmpty()) {
-                url = new URL(baseUrl, baseProject + "/issues/" + issueNumber);
+            if (matcher.group("otherProject").toString() != null && ! matcher.group("otherProject").toString().isEmpty()) {
+                url = new URL(baseUrl, matcher.group("otherProject").toString() + "/issues/" + issueNumber);
             } else {
-                url = new URL(baseUrl, matcher.group(4).toString() + "/issues/" + issueNumber);
+                url = new URL(baseUrl, baseProject + "/issues/" + issueNumber);
             }
             final Issue issue = new Issue(type, issueNumber, url);
             log.info("Found issue " + issue.getIssue() + " with transition type " + issue.getTransition() + " and URL to the issue " + issue.getUrl().toString());
--- src/test/java/net/praqma/utils/parsers/cmg/impl/GitHubTest.java
+++ src/test/java/net/praqma/utils/parsers/cmg/impl/GitHubTest.java
@@ -3,7 +3,6 @@ package net.praqma.utils.parsers.cmg.impl;
 import net.praqma.utils.parsers.cmg.api.CommitMessageParser;
 import net.praqma.utils.parsers.cmg.api.Issue;
 import net.praqma.utils.parsers.cmg.api.TransitionType;
-import net.praqma.utils.parsers.cmg.impl.GitHub;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,18 +17,113 @@ public class GitHubTest {
         final URL baseUrl = new URL("http://github.com");
         final String baseProject = "user/repo";
         final CommitMessageParser parser = new GitHub(baseUrl, baseProject);
+        // magic word fix and ignored text
         List<Issue> issues = parser.parse("Fix #123: my super fix");
         assertEquals(1, issues.size());
         assertEquals(TransitionType.RESOLVE, issues.get(0).getTransition());
         assertEquals("123", issues.get(0).getIssue());
         assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
 
-        issues = parser.parse("ignored text #123 in my super fix");
+        // magic word close and no ignore text
+        issues = parser.parse("close #123");
+        assertEquals(1, issues.size());
+        assertEquals(TransitionType.RESOLVE, issues.get(0).getTransition());
+        assertEquals("123", issues.get(0).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
+
+        // magic word close surrounded by ignored text
+        issues = parser.parse("ignored text close #123 ignored text");
+        assertEquals(1, issues.size());
+        assertEquals(TransitionType.RESOLVE, issues.get(0).getTransition());
+        assertEquals("123", issues.get(0).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
+
+        // magic word revert surrounded by ignored text
+        issues = parser.parse("ignored text revert #123 ignored text");
+        assertEquals(1, issues.size());
+        assertEquals(TransitionType.REVERT, issues.get(0).getTransition());
+        assertEquals("123", issues.get(0).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
+
+        // magic word resolve surrounded by ignored text
+        issues = parser.parse("ignored text revert #123 ignored text");
+        assertEquals(1, issues.size());
+        assertEquals(TransitionType.REVERT, issues.get(0).getTransition());
+        assertEquals("123", issues.get(0).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
+
+        // only issue
+        issues = parser.parse("#123");
         assertEquals(1, issues.size());
         assertEquals(TransitionType.REFERENCE, issues.get(0).getTransition());
         assertEquals("123", issues.get(0).getIssue());
         assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
 
+        // only issue and ignored text
+        issues = parser.parse("text #123");
+        assertEquals(1, issues.size());
+        assertEquals(TransitionType.REFERENCE, issues.get(0).getTransition());
+        assertEquals("123", issues.get(0).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
+
+        // incorrect issue
+        issues = parser.parse("fix #a123");
+        assertEquals(0, issues.size());
+
+        // test multiline
+        String multiline = new StringBuilder()
+                .append("ignored text\n")
+                .append("ignored text resolve #123 ignored text\n")
+                .toString();
+        issues = parser.parse(multiline);
+        assertEquals(1, issues.size());
+        assertEquals(TransitionType.RESOLVE, issues.get(0).getTransition());
+        assertEquals("123", issues.get(0).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
+
+        // issue and magic word should be on the same line
+        multiline = new StringBuilder()
+                .append("ignored text\n")
+                .append("ignored text fix\n")
+                .append("#123 ignored text\n")
+                .toString();
+        issues = parser.parse(multiline);
+        assertEquals(1, issues.size());
+        assertEquals(TransitionType.REFERENCE, issues.get(0).getTransition());
+        assertEquals("123", issues.get(0).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
+
+        // two issues multiline
+        multiline = new StringBuilder()
+                .append("ignored text\n")
+                .append("ignored text #123 ignored text\n")
+                .append("ignored text #345 ignored text\n")
+                .toString();
+        issues = parser.parse(multiline);
+        assertEquals(2, issues.size());
+        assertEquals(TransitionType.REFERENCE, issues.get(0).getTransition());
+        assertEquals("123", issues.get(0).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
+        assertEquals(TransitionType.REFERENCE, issues.get(1).getTransition());
+        assertEquals("345", issues.get(1).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/345").toString(), issues.get(1).getUrl().toString());
+
+        // two issues and two key words multiline
+        multiline = new StringBuilder()
+                .append("ignored text\n")
+                .append("ignored text resolves #123 ignored text\n")
+                .append("ignored text reopen #345 text #revert\n")
+                .toString();
+        issues = parser.parse(multiline);
+        assertEquals(2, issues.size());
+        assertEquals(TransitionType.RESOLVE, issues.get(0).getTransition());
+        assertEquals("123", issues.get(0).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(0).getUrl().toString());
+        assertEquals(TransitionType.REVERT, issues.get(1).getTransition());
+        assertEquals("345", issues.get(1).getIssue());
+        assertEquals(new URL(baseUrl, baseProject + "/issues/345").toString(), issues.get(1).getUrl().toString());
+
+        // two issues and two keywords one line
         issues = parser.parse("Fix #345 and mention #123 in my super fix");
         assertEquals(2, issues.size());
         assertEquals(TransitionType.RESOLVE, issues.get(0).getTransition());
@@ -39,6 +133,7 @@ public class GitHubTest {
         assertEquals("123", issues.get(1).getIssue());
         assertEquals(new URL(baseUrl, baseProject + "/issues/123").toString(), issues.get(1).getUrl().toString());
 
+        // refer other project
         issues = parser.parse("Fix #345 and mention otheruser/repo#123 in my super fix");
         assertEquals(2, issues.size());
         assertEquals(TransitionType.RESOLVE, issues.get(0).getTransition());
